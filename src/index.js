65cab33e7cf5818cfec4dec614d4b4a3f3d14ccf
Hannes Rüger <hannesrueger@gmx.de>
2020-10-03 13:20:37 +0200

    added some logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Started");
+const mqtt = require("mqtt");
+const client  = mqtt.connect(`mqtt://${process.env.MQTT_HOST || "mqtt"}:${process.env.MQTT_PORT || 1883}`);
+ 
+client.on("connect", function () {
+    console.log("Connected successfully!");
+    const topic = `${process.env.MQTT_HOST || "datalog"}`;
+    client.subscribe(topic, function (err) {
+        if (err) {
+            console.log(`Error while subscribing: ${err}`);
+            return;
+        }
+        console.log(`Successfully subscribed to ${topic}!`);
+    })
+})
+ 
+client.on("message", function (topic, message) {
+    console.log(`Incoming message: ${message.toString()}`);
+})
